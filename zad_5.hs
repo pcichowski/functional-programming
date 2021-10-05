8306ec8ae6ac26f5f7cf2c0fd5e7378e1de247d7
Paweł Cichowski <81694867+Silentsky0@users.noreply.github.com>
2021-10-05 23:22:00 +0200

    Cleaned up the code, added descriptive comments

--- zad_5.hs
+++ zad_5.hs
@@ -1,33 +1,32 @@
 module Main where
-
 main :: IO ()
-main = do
- let dupa = [ [1], [2] ]
-  in  putStrLn ( printListElements (driver dupa) )
- n <- getLine 
+main =
  return ()
- 
-{- TODO linijka 5 -}
 
-{- driver obsługuje każdy element listy od jej końca -}
+{-  driver dziala na dwoch kopiach listy, 
+    do jednej z nich dodaje element zbioru do kazdego elementu listy, 
+    następnie konkatenuje z drugą kopią     -}
 driver :: [[a]] -> [[a]]
 driver [] = [[]]
-driver [n] = [[] , n]
 driver (n:ls) =
  driver ls ++ dodajDoKazdego (driver ls) n
 
-{-  dodajDoKazdego dodaje element np. [8] do listy list np [ [], [1], [2] ] -> [ [8], [1, 8], [2, 8] ] -}
+{-  dodajDoKazdego dodaje element np. [8] do kazdego elementu listy,
+    np [ [], [1], [2] ] -> [ [8], [1, 8], [2, 8] ]      -}
 dodajDoKazdego :: [[a]] -> [a] -> [[a]]
 dodajDoKazdego [] _ = []
 dodajDoKazdego (n:ls) elem =
  [n ++ elem] ++ dodajDoKazdego ls elem
 
+{-  konwertuj zamienia postać listy na listę singletonów
+    np [1, 2, 3] -> [[1], [2], [3]]     -}
 konwertuj :: [a] -> [[a]]
 konwertuj [] = [[]]
 konwertuj [x] = [[x]]
 konwertuj (n:ls) =
   [n] : konwertuj ls
 
+{-  printListElements zamienia listę na zwykłego stringa    -}
 printListElements :: Show a => [[a]] -> String
 printListElements [[]] = ""
 printListElements [] = ""
