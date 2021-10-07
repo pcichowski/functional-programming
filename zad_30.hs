b4144ff06552d6b2534bbcb3c7db37be5e2b9952
Paweł Cichowski <81694867+Silentsky0@users.noreply.github.com>
2021-10-07 17:24:24 +0200

    Added usunDuplikaty function

--- zad_30.hs
+++ zad_30.hs
@@ -2,3 +2,14 @@ module Main where
 main :: IO ()
 main =
  return ()
+
+{- input: [[1,1,1], [2,2], [3], [4, 4, 4], [5]] 
+   output: [ [3], [5] ]     -}
+
+usunDuplikaty :: [[a]] -> [[a]]
+usunDuplikaty [] = []
+usunDuplikaty [[]] = [[]]
+usunDuplikaty (x:xs) =
+ if length x > 1
+  then  usunDuplikaty xs
+ else x : usunDuplikaty xs
