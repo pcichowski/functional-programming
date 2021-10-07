988b347fff5e72f92b1027848c14b040348f135f
kapselccc <74978589+kapselccc@users.noreply.github.com>
2021-10-07 19:20:43 +0200

    Update zad_5.hs

--- zad_5.hs
+++ zad_5.hs
@@ -10,13 +10,10 @@ driver :: [[a]] -> [[a]]
 driver [] = [[]]
 driver (n:ls) =
  driver ls ++ dodajDoKazdego (driver ls) n
-
-{-  dodajDoKazdego dodaje element np. [8] do kazdego elementu listy,
-    np [ [], [1], [2] ] -> [ [8], [1, 8], [2, 8] ]      -}
 dodajDoKazdego :: [[a]] -> [a] -> [[a]]
 dodajDoKazdego [] _ = []
 dodajDoKazdego (n:ls) elem =
- [n ++ elem] ++ dodajDoKazdego ls elem
+ (n ++ elem) : dodajDoKazdego ls elem
 
 {-  konwertuj zamienia postać listy na listę singletonów
     np [1, 2, 3] -> [[1], [2], [3]]     -}
@@ -30,5 +27,9 @@ konwertuj (n:ls) =
 printListElements :: Show a => [[a]] -> String
 printListElements [[]] = ""
 printListElements [] = ""
-printListElements (n:ls) = 
+printListElements (n:ls) =
  show n ++ " " ++ printListElements ls
+
+{-  zbiorPotegowy to końcowa funkcja która przyjmuję listę i zwraca zbiór potegowy-}
+zbiorPotegowy :: [a] -> [[a]]
+zbiorPotegowy x = driver (konwertuj x) 
