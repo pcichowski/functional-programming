133739e7c98d539021b32ffef9bc6184777799be
Paweł Cichowski <81694867+Silentsky0@users.noreply.github.com>
2021-10-07 19:00:06 +0200

    Added grupuj function

--- zad_30.hs
+++ zad_30.hs
@@ -14,8 +14,28 @@ usunDuplikaty (x:xs) =
   then  usunDuplikaty xs
  else x : usunDuplikaty xs
 
+grupuj' :: [[a]] -> [[a]]
+grupuj' (x:xs) = do
+ {-until (head xs == head x) (przeniesPierwszyElement x xs)-}
+ x : grupuj' xs
+
+grupuj :: Eq a => [a] -> [[a]]
+grupuj = 
+ foldr func []
+   
+func :: Eq a => a -> [[a]] -> [[a]]
+func x [] = [[x]]
+func x (y:xs) = 
+ if x == head y 
+  then (x:y):xs 
+ else [x]:y:xs
+
+przeniesPierwszyElement :: [a] -> [[a]] -> [a]
+przeniesPierwszyElement xs (y:ys) =
+ xs ++ y
+
 rozkrkrotkuj :: [(a,a)] -> [a]
 rozkrkrotkuj [] = []
-rozkrkrotkuj (x:xs) = fst x : [snd x] ++ rozkrkrotkuj xs
+rozkrkrotkuj (x:xs) = fst x : snd x : rozkrkrotkuj xs
 
 
