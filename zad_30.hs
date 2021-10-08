fe5be2a7aab127ef3383987ceafa6ea24d5a7773
Paweł Cichowski <81694867+Silentsky0@users.noreply.github.com>
2021-10-08 14:57:45 +0200

    Added functions' descriptions

--- zad_30.hs
+++ zad_30.hs
@@ -3,9 +3,8 @@ main :: IO ()
 main =
  return ()
 
-{- input: [[1,1,1], [2,2], [3], [4, 4, 4], [5]] 
-   output: [ [3], [5] ]     -}
-
+{- funkcja usuwa listy elementow których liczba jest wieksza niż 1
+   np. [[1,1,1], [2,2], [3], [4, 4, 4], [5]] ==>  [[3], [5]] -}
 usunDuplikaty :: [[a]] -> [[a]]
 usunDuplikaty [] = []
 usunDuplikaty [[]] = [[]]
@@ -14,18 +13,21 @@ usunDuplikaty (x:xs) =
   then  usunDuplikaty xs
  else x : usunDuplikaty xs
 
-grupuj' :: [[a]] -> [[a]]
-grupuj' (x:xs) = do
- {-until (head xs == head x) (przeniesPierwszyElement x xs)-}
- x : grupuj' xs
-
+{- funkcja grupuje elementy w krotki
+   np. [1, 1, 1 , 2, 2, 2, 2, 3]  ==>  [[1, 1, 1], [2, 2, 2, 2], [3]] 
+   identyczna funkcja: 
+   grupuj lista = 
+    foldr dodajJezeliRowne [] lista    -}
 grupuj :: Eq a => [a] -> [[a]]
 grupuj = 
- foldr func []
-   
-func :: Eq a => a -> [[a]] -> [[a]]
-func x [] = [[x]]
-func x (y:xs) = 
+ foldr dodajJezeliRowne []
+
+{- funkcja doda element x do listy list jeżeli składa się ona z elementów x 
+   np. dodajJezeliRowne 1 [[1, 1, 1], [2, 2], [1, 1]]  ==>  [[1, 1, 1, 1], [2, 2], [1, 1]] 
+   np. dodajJezeliRowne 3 [[5, 5, 5], [4], [3, 3]]   ==>  [[3], [5, 5, 5], [4], [3, 3]]-}
+dodajJezeliRowne :: Eq a => a -> [[a]] -> [[a]]
+dodajJezeliRowne x [] = [[x]]
+dodajJezeliRowne x (y:xs) = 
  if x == head y 
   then (x:y):xs 
  else [x]:y:xs
@@ -34,6 +36,8 @@ przeniesPierwszyElement :: [a] -> [[a]] -> [a]
 przeniesPierwszyElement xs (y:ys) =
  xs ++ y
 
+{- funkcja zamienia listę krotek na listę elementów w tej samej kolejności
+   np. [(1, 2), (1, 7), (8, 6)] ==> [1, 2, 1, 7, 8 ,6] -}
 rozkrotkuj :: [(a,a)] -> [a]
 rozkrotkuj [] = []
 rozkrotkuj (x:xs) = fst x : [snd x] ++ rozkrotkuj xs
