a9f00b8566e6226e5db6dd873f98a8bc3dbdd4dc
kapselccc <74978589+kapselccc@users.noreply.github.com>
2021-10-10 19:25:44 +0200

    iterate on list

--- zad_21.hs
+++ zad_21.hs
@@ -7,7 +7,7 @@ isPrimal n = if n > 1 then isEmpty [x | x <- [2..(n `div` 2)], n `mod` x == 0]  
 
 isEmpty :: [x] -> Bool
 isEmpty [] = True
-isEmpty xs = False
+isEmpty x = False
 
 createPrimalList :: Int -> [Int]
 createPrimalList n = [x | x <- [2..n], isPrimal x]
@@ -15,3 +15,12 @@ createPrimalList n = [x | x <- [2..n], isPrimal x]
 numberToListOfDigits :: Int -> [Int]
 numberToListOfDigits 0 = []
 numberToListOfDigits n = numberToListOfDigits(n `div` 10) ++ [n `mod` 10]
+
+iterateOnList :: [Int] -> [Int]
+iterateOnList x = tail x ++ [head x]
+
+iterateTest :: [Int] -> Int -> IO()
+iterateTest x 0 = return()
+iterateTest x n = do
+    putStrLn (show (iterateOnList x))
+    iterateTest (iterateOnList x) (n-1)
