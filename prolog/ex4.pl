869be5a69010d8f3fb885fc1b13d258c76baf11a
Paweł Cichowski <81694867+Silentsky0@users.noreply.github.com>
2021-11-23 22:14:41 +0100

    Added permutation generation

--- prolog/ex4.pl
+++ prolog/ex4.pl
@@ -16,4 +16,10 @@ tworz_plansze(N, M, Plansza) :-
     random_permutation(Polaczone_plansze, Plansza).
     
 
+takeout(X,[X|R],R).  
+takeout(X,[F |R],[F|S]) :- takeout(X,R,S).
+
+perm([X|Y],Z) :- perm(Y,W), takeout(X,Z,W).  
+perm([],[]).
+
     
