b5557daf75f104faf5cd39efc79981fad1e2518f
Paweł Cichowski <81694867+Silentsky0@users.noreply.github.com>
2021-11-23 16:09:28 +0100

    Added board creation with random pawn generation

--- prolog/ex4.pl
+++ prolog/ex4.pl
@@ -1,16 +1,19 @@
 :- include('ex1-test.pl').
 
-tworz_plansze(N, Pusta_Plansza) :-
+tworz_plansze_zer(N, Pusta_Plansza) :-
     length(Pusta_Plansza, N),
     maplist(=(0), Pusta_Plansza).
+tworz_plansze_jedynek(N, Plansza) :-
+    length(Plansza, N),
+    maplist(=(1), Plansza).
 
-
-dodaj_pionka_posortuj(Plansza, Output) :-
-    random_member(X, Plansza),
-    X is 1,
-    sortuj(Plansza, Output).
+% N - dlugosc planszy, M - liczba pionkow
+tworz_plansze(N, M, Plansza) :-
+    Liczba_zer is N - M,
+    tworz_plansze_zer(Liczba_zer, Plansza_1),
+    tworz_plansze_jedynek(M, Plansza_2),
+    append(Plansza_1, Plansza_2, Polaczone_plansze),
+    random_permutation(Polaczone_plansze, Plansza).
     
-
-populuj_plansze(M, Plansza).
 
     
